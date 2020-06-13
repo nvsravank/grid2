c24e015b1813fcd226a3d2d785cae0b223d253c7
Sravan Nerella <nvsravank@yahoo.com>
2020-06-13 16:02:25 -0400

    .

--- projects/report-builder/src/app/utilities/http-error.interceptor.ts
+++ projects/report-builder/src/app/utilities/http-error.interceptor.ts
@@ -16,7 +16,8 @@ export class HttpErrorInterceptor implements HttpInterceptor {
             // server-side error
             errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
           }
-          window.alert(errorMessage);
+          // window.alert(errorMessage); // Best practice is to use a more graceful display of the error message rather than window alert.
+          // Also consider this area for logging of errors with a server based logger so as to enable better error tracking of front end.
           return throwError(errorMessage);
         })
       )
